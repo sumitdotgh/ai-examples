e823ed113acb3f46c1e6aac681838ecbb0b61786
sumitdotgh <sughos@outlook.com>
2025-06-18 09:46:41 +0530

    first draft of the basic agent 2 agent implementation

---
+++ basic-agent-2-agent/client.py
@@ -0,0 +1,21 @@
+from typing import Any
+from uuid import uuid4
+
+import httpx
+
+from a2a.client import A2ACardResolver, A2AClient
+from a2a.types import (
+    AgentCard,
+    MessageSendParams,
+    SendMessageRequest,
+    SendStreamingMessageRequest,
+)
+
+PUBLIC_AGENT_CARD_PATH = '/.well-known/agent.json'
+
+async def main() -> None:
+    pass
+
+if __name__ == '__main__':
+
+    pass
---
+++ basic-agent-2-agent/weather-agent/main.py
@@ -0,0 +1,52 @@
+import uvicorn
+import asyncio
+
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import (
+    AgentCard,
+    AgentSkill,
+    AgentCapabilities
+)
+from .agent_executor import (
+    WeatherAgentExecutor
+)
+
+async def main() -> None:
+    
+    skill = AgentSkill(
+        id='weather_agent',
+        name='Weather specific information',
+        description='Share weather specific information',
+        tags=['weather info'],
+        examples=['What is the weather of blr?'],
+    )
+    
+    agent_card = AgentCard(
+        name="Weather agent",
+        description="A simple agent that returns weather information",
+        url="http://localhost:9999",
+        defaultInputModes=["text"],
+        defaultOutputModes=["text"],
+        skills=[skill],
+        version="1.0.0",
+        capabilities=AgentCapabilities()
+    )
+
+    request_handler = DefaultRequestHandler(
+        agent_executor=WeatherAgentExecutor(),
+        task_store=InMemoryTaskStore(),
+    )
+
+    server = A2AStarletteApplication(
+        agent_card=agent_card,
+        http_handler=request_handler,        
+    )
+
+    uvicorn.run(server.build(), host='0.0.0.0', port=9999)
+
+
+if __name__ == '__main__':
+
+    asyncio.run(main())
